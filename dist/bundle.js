e103e067eb6ef36615b304c156afc9c961c06f8b
Jordan Castelloe <jordan.castelloe@gmail.com>
2017-12-11 06:03:34 -0600

    fixed bug in for loop

--- dist/bundle.js
+++ dist/bundle.js
@@ -9,6 +9,8 @@ function loadCarnivores (callbackToInvoke) {
 
     request.addEventListener("load", function () {
         carnivores = JSON.parse(event.target.responseText);
+        carnivores = carnivores.animals;
+        console.log("carnivore array", carnivores); // this works
         callbackToInvoke(carnivores);
     });
     request.open("GET", "./scripts/carnivores.json");
@@ -20,6 +22,8 @@ function loadHerbivores (callbackToInvoke) {
 
     request.addEventListener("load", function () {
         herbivores = JSON.parse(event.target.responseText);
+        herbivores = herbivores.animals;
+        console.log("herbivore array", herbivores); // this also works
         callbackToInvoke(herbivores);
     });
     request.open("GET", "./scripts/herbivores.json");
@@ -31,17 +35,22 @@ module.exports = {loadCarnivores, loadHerbivores};
 },{}],2:[function(require,module,exports){
 "use strict";
 
-console.log("hello!");
-
 const animalLoader = require("./animal");
+
 const carnivoreContainer = document.getElementById("carnivore-container");
 const herbivoreContainer = document.getElementById("herbivore-container");
 
+console.log("show carnivore function", animalLoader.loadCarnivores);
+console.log("show herbivore function", animalLoader.loadHerbivores);
 
 function showCarnivores(carnivores) {
+    console.log("the showCarnivores callback function was fired!");
+    console.log("this should be the carnivores array", carnivores);
+    console.log("carnivores.length", carnivores.length);
     for (let i = 0; i < carnivores.length; i++){
         let parentNode = document.createElement("p");
-        let animalName = document.createTextNode(carnivores[i][name]);
+        let animalName = document.createTextNode(carnivores[i].name);
+        console.log(carnivores[i].name);
         parentNode.appendChild(animalName);
         carnivoreContainer.appendChild(parentNode);
     }
@@ -50,12 +59,13 @@ function showCarnivores(carnivores) {
 function showHerbivores(herbivores) {
     for (let i = 0; i < herbivores.length; i++) {
         let parentNode = document.createElement("p");
-        let animalName = document.createTextNode(herbivores[i][name]);
+        let animalName = document.createTextNode(herbivores[i].name);
         parentNode.appendChild(animalName);
         herbivoreContainer.appendChild(parentNode);
     }
     
 }
+
 
 animalLoader.loadCarnivores(showCarnivores);
 animalLoader.loadHerbivores(showHerbivores);
--- scripts/animal.js
+++ scripts/animal.js
@@ -8,6 +8,8 @@ function loadCarnivores (callbackToInvoke) {
 
     request.addEventListener("load", function () {
         carnivores = JSON.parse(event.target.responseText);
+        carnivores = carnivores.animals;
+        console.log("carnivore array", carnivores); // this works
         callbackToInvoke(carnivores);
     });
     request.open("GET", "./scripts/carnivores.json");
@@ -19,6 +21,8 @@ function loadHerbivores (callbackToInvoke) {
 
     request.addEventListener("load", function () {
         herbivores = JSON.parse(event.target.responseText);
+        herbivores = herbivores.animals;
+        console.log("herbivore array", herbivores); // this also works
         callbackToInvoke(herbivores);
     });
     request.open("GET", "./scripts/herbivores.json");
